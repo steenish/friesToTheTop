20984a7d4a4b1681b939ed2bda4e9c19c3a65a24
Geodrop Studios <geodropstudios@gmail.com>
2020-07-25 19:54:54 +0200

    create fog controller

---
+++ freebirb/Assets/Scripts/FogController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerTransform;
+    [SerializeField]
+    private float fogPlayerOffset;
+
+    private float playerMaxAltitude;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        playerMaxAltitude = (playerTransform.position.y > playerMaxAltitude) ? playerTransform.position.y : playerMaxAltitude;
+        transform.position = new Vector3(playerTransform.position.x, playerMaxAltitude - fogPlayerOffset, playerTransform.position.z);
+    }
+}
