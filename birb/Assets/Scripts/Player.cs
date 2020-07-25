4d3c5092f00956434c3bd14db1b0b70ca46b9f44
Anton Lövström <anton.lovstrom4@gmail.com>
2020-07-25 13:25:44 +0200

    Add very basic gliding physics

---
+++ birb/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float forceMultiplier;
+
+    private Rigidbody rb;
+    private Vector2 input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Gliding physics
+        rb.AddForce(transform.up * forceMultiplier * (Mathf.Clamp((90f - transform.rotation.eulerAngles.x), 0, 90f) / 90f));
+        rb.AddForce(transform.forward * forceMultiplier * Mathf.Clamp((transform.rotation.eulerAngles.x / 90f), 0, 1));
+    }
+}
