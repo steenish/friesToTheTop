65b7784d145cee92b7cda35037dc580ba461d3ed
Anton Lövström <anton.lovstrom4@gmail.com>
2020-07-25 15:44:16 +0200

    Add camera rotation to always look towards player

--- birb/Assets/Scripts/Camera.cs
+++ birb/Assets/Scripts/Camera.cs
@@ -6,17 +6,18 @@ public class Camera : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 cameraOffset;
+    private float offsetDistance;
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraOffset = transform.position - player.transform.position;
+        offsetDistance = (transform.position - player.transform.position).magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + cameraOffset;
+        transform.position = player.transform.position + player.transform.forward * -1 * offsetDistance;
+        transform.LookAt(player.transform.position);
     }
 }
