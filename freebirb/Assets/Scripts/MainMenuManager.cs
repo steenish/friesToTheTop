6d04df7451e19da545c7986e4347004f2df1093d
Anders Steen <anders.steen98@gmail.com>
2020-07-26 10:45:19 +0200

    create main menu scene

---
+++ freebirb/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+
+#pragma warning disable
+    [SerializeField]
+    private Text highscoreText;
+#pragma warning restore
+
+    void Start() {
+        highscoreText.text = PlayerPrefs.HasKey("highscore") ? "Highscore: 0" : "Highscore: " + PlayerPrefs.GetInt("highscore");
+    }
+
+    void Update() {
+        
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("MainGameScene");
+    }
+}
