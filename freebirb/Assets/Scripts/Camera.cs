620336558cee51840c38fff272407267d1f06abe
Anton Lövström <anton.lovstrom4@gmail.com>
2020-07-26 23:35:42 +0200

    Lock camera rotation

--- freebirb/Assets/Scripts/Camera.cs
+++ freebirb/Assets/Scripts/Camera.cs
@@ -3,22 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Camera : MonoBehaviour {
-
-#pragma warning disable
-    [SerializeField]
-    private GameObject player;
-    [SerializeField]
-    private float heightOffset;
-#pragma warning restore
-
-    private float offsetDistance;
-
-    void Start() {
-        offsetDistance = (transform.position - player.transform.position).magnitude;
-    }
-
     void Update() {
-        transform.position = player.transform.position + player.transform.forward * -1 * offsetDistance + player.transform.up * heightOffset;
-        transform.LookAt(player.transform.position);
+        Quaternion rotation = transform.rotation;
+            Vector3 eulerRotation = rotation.eulerAngles;
+            eulerRotation.z = 0f;
+            rotation.eulerAngles = eulerRotation;
+            transform.rotation = rotation;
     }
 }
